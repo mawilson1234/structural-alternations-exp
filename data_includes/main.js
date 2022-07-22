abe647289d5fdd8e5114b87b7da1b995299b60ab
mawilson1234 <michael.a.wilson@yale.edu>
2022-07-21 21:51:48 -0400

    test dragdrop

--- data_includes/main.js
+++ data_includes/main.js
@@ -90,9 +90,9 @@ var feedback_trial = label => item => {
 	return newTrial(label,		
 		newText("container", "").center().css({display: "flex", 'margin-bottom': '3em'}).print(),
 		newText(presentence).print(getText("container")),
-		newText(first_arg, " ").css(blank_style).print(getText("container")),
+		newText(first_arg, "").css(blank_style).print(getText("container")),
 		newText(midsentence).print(getText("container")),
-		newText(second_arg, " ").css(blank_style).print(getText("container")),
+		newText(second_arg, "").css(blank_style).print(getText("container")),
 		newText(postsentence).print(getText("container")),
 		
 		newText("placeholder", "&mdash;").center().print(),
@@ -123,7 +123,10 @@ var feedback_trial = label => item => {
 						correct = true
 					)
 					.failure(getText("incorrect").print()),
-					getText("word").css({padding: '', width: '5em', 'text-align': 'center', 'margin-left': '-0.5em'})
+					getText("word").css({
+						padding: '', width: '5em', 
+						'text-align': 'center'
+					})
 			)
 			.offset('0.5em', '0.1em', getText(first_arg), getText(second_arg))
 			.wait(self.test.dropped(getText(target_res)))
