8ef4e820daefb99e47eaa634948fdd4f7493e577
mawilson1234 <michael.a.wilson@yale.edu>
2022-07-21 21:55:19 -0400

    test dragdrop

--- data_includes/main.js
+++ data_includes/main.js
@@ -126,7 +126,7 @@ var feedback_trial = label => item => {
 					getText("word").css({
 						padding: '', width: '5em', 
 						'text-align': 'center',
-						'margin-left': '-0.54em', 'margin-top': '-3px'
+						'margin-left': '-0.54em', 'margin-top': '-1.5px'
 					})
 			)
 			.offset('0.5em', '0.1em', getText(first_arg), getText(second_arg))
