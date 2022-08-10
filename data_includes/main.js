e23241754694f0e30a570bd7dd002c9f8dc64bfe
mawilson1234 <michael.a.wilson@yale.edu>
2022-08-10 16:16:53 -0400

    fix items between breaks

--- data_includes/main.js
+++ data_includes/main.js
@@ -57,7 +57,7 @@ Sequence(
 	"instruction2",
 	randomize("trial_train"),
 	"instruction3",
-	sepWithN('break', randomize("trial"), 54),
+	sepWithN('break', randomize("trial"), 57),
 	"feedback",
 	SendResults(),
 	"bye"
