3fb2b55f129d3ccca40b4913feaff98e0106531f
Michael Wilson <michael.a.wilson@yale.edu>
2022-09-21 08:56:07 -0400

    update script

--- scripts/power_analysis.sh
+++ scripts/power_analysis.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
