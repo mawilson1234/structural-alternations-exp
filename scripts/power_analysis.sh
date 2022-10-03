cf8f92aa4cc16bca5f304b72227324d711203c7e
Michael Wilson <michael.a.wilson@yale.edu>
2022-10-02 21:59:07 -0400

    update permissions

--- scripts/power_analysis.sh
+++ scripts/power_analysis.sh
@@ -3,10 +3,11 @@
 #SBATCH --job-name=salts_exp_power_analysis
 #SBATCH --output=joblogs/%x_%j.txt
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --partition=week
+#SBATCH --time=7-00:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
@@ -15,4 +16,4 @@ echo Running script: scripts/salts_exp_power_analysis.sh
 
 cd analysis/
 
-Rscript power_analysis.r
+Rscript power_analysis-freq.r
