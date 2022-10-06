5f072d75f91a27ab3b4b51eedf66b4d5e6f53777
Michael Wilson <michael.a.wilson@yale.edu>
2022-10-06 01:02:46 -0400

    permissions

--- scripts/bayesian_analysis.sh
+++ scripts/bayesian_analysis.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --partition=week
-#SBATCH --time=32:00:00
+#SBATCH --time=07-00:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
