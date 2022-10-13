75bfb1ca995ce6eaa9ed10ab53d883cfcfbc2c85
mawilson1234 <michael.a.wilson@yale.edu>
2022-10-13 18:23:08 -0400

    update scripts

--- analysis/Bayesian scripts/crossed_model_RTs.r
+++ analysis/Bayesian scripts/crossed_model_RTs.r
@@ -1,15 +1,27 @@
-library(brms)
-library(dplyr)
+source(file.path(dirname(sys.frame(1)$ofile), 'summary_functions.r'))
 
 models.dir <- 'Models/Bayesian'
 dir.create(models.dir, showWarnings=FALSE, recursive=TRUE)
+
+MAX_RT_IN_SECONDS <- 10
+OUTLIER_RT_SDS <- 2
 
 results <- read.csv('accuracy-data.csv') |>
 	mutate(
 		subject = as.factor(subject),
 		item 	= as.factor(item)
 	) |>
-	filter(data_source == 'human') |>
+	filter(
+		data_source == 'human',
+		log.RT < log(MAX_RT_IN_SECONDS*1000)
+	) |>
+	group_by(subject) |>
+	mutate(sd.log.RT = sd(log.RT)) |>
+	filter(
+		log.RT < mean(log.RT) + (OUTLIER_RT_SDS * sd.log.RT), 
+		log.RT > mean(log.RT) - (OUTLIER_RT_SDS * sd.log.RT)
+	) |>
+	ungroup() |>
 	select(-data_source, -data_source.n) |>
 	droplevels()
 
@@ -40,7 +52,9 @@ brm.args <- list(
 	seed=425, refresh=1
 )
 
-do.call(brm, append(brm.args, list(
+models <- list()
+
+models['Crossed model (RTs)'] <- do.call(brm, append(brm.args, list(
 	formula = RT ~ voice.n * target_response.n * seen_in_training.n * linear.n +
 		(1 + voice.n * target_response.n * seen_in_training.n | linear.n:subject) +
 		(1 + linear.n | voice.n:target_response.n:seen_in_training.n:item),
@@ -49,3 +63,16 @@ do.call(brm, append(brm.args, list(
 	prior = priors_crossed_RT,
 	file = file.path(models.dir, 'crossed_model_RTs.rds')
 )))
+
+save_model_summaries(
+	models,
+	filename='crossed_model_RTs_summary.txt', 
+	overwrite=TRUE
+)
+
+save_pmcmc(
+	models,
+	filename='crossed_model_RTs_pmcmcs.txt'
+)
+
+save_model_plots(models)
