a31596e2c528e05be45713d1470c1399d0a6f689
mawilson1234 <michael.a.wilson@yale.edu>
2022-10-21 12:02:00 -0400

    update script running times

--- scripts/bayesian_analysis/bayesian_sims_crossed_non-linear.sh
+++ scripts/bayesian_analysis/bayesian_sims_crossed_non-linear.sh
@@ -5,8 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --mem=64G
+#SBATCH --partition=week
+#SBATCH --time=07-00:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
