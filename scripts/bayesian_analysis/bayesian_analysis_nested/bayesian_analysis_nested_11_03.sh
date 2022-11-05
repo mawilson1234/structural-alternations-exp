fe7fcd1bdfbadba0c894ba4c51cb5de9126f7cb2
mawilson1234 <michael.a.wilson@yale.edu>
2022-11-05 15:07:27 -0400

    update running time due to job failures

--- scripts/bayesian_analysis/bayesian_analysis_nested/bayesian_analysis_nested_11_03.sh
+++ scripts/bayesian_analysis/bayesian_analysis_nested/bayesian_analysis_nested_11_03.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=05:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.1.0-foss-2020b
